226247fe8cc0fe8b0300a84dbfb844ade1d3fe27
JM Baldonado <john.baldonado@xurpas.com>
2023-01-13 22:21:42 +0800

    implement manufacturer struct and contains trait

--- car-manufacturer/src/main.rs
+++ car-manufacturer/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+#![deny(clippy::all)]
+
+use std::env;
+
+const API_URL: &str = "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json";
+
+struct Manufacturer<'a> {
+    name: Option<&'a str>,
+    common_name: Option<&'a str>,
+    country: Option<&'a str>,
+}
+
+trait Contains {
+    fn contains(&self, needle: &str) -> bool;
+}
+
+impl<'a> Contains for Manufacturer<'a> {
+    fn contains(&self, needle: &str) -> bool {
+        self.name.unwrap_or_default().contains(needle) || self.common_name.unwrap_or_default()
+            .contains(needle) || self.country.unwrap_or_default().contains(needle)
+    }
+}
+    fn main() {
     println!("Hello, world!");
 }
